125ec07ee53495608ab5e77807662b011b1f398f
fernandezja <fernandezja@gmail.com>
2020-03-31 11:56:01 -0300

    Greeter service with SayHello (client/server)

---
+++ src/Starwars.Grpc.Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Net.Client;
+using Starwars.Grpc.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace Starwars.Grpc.Client
+{
+    class Program
+    {
+        private const string ENDPOINT = "https://localhost:5001";
+
+        static async Task Main(string[] args)
+        {
+
+            using (var channel = GrpcChannel.ForAddress(ENDPOINT))
+            {
+                //option csharp_namespace = "Starwars.Grpc.Server";
+                var client = new Greeter.GreeterClient(channel);
+
+                var helloRequest = new HelloRequest()
+                {
+                    Name = "Yoda"
+                };
+
+                var helloReply = await client.SayHelloAsync(helloRequest);
+
+                Console.WriteLine(helloReply.Message);
+
+            }
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+        }
+    }
+}
